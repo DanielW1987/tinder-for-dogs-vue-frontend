9d5c381deed7d6a7c7399f4755f14051918eeb4c
Daniel Wagner <wagner.daniel87@gmail.com>
2021-10-10 14:36:47 +0200

    static like or dislike component

--- src/views/LikeOrDislike.vue
+++ src/views/LikeOrDislike.vue
@@ -1,6 +1,103 @@
 <template>
   <h1>Like or Dislike</h1>
+  <div class="row">
+    <img
+      src="https://www.deine-tierwelt.de/services/wp-content/uploads/sites/3/2020/06/rufus-laborbeagle-auf-sofa.jpg"
+      class="rounded mx-auto d-block dog-photo"
+      alt="Dog photo"
+    >
+  </div>
+  <div class="row">
+    <h2 class="text-center">Rufus</h2>
+  </div>
+  <div class="row">
+    <div class="col-6 text-end">
+      <button type="button" class="btn btn-danger .btn-lg">Dislike</button>
+    </div>
+    <div class="col-6">
+      <button type="button" class="btn btn-success .btn-lg">Like</button>
+    </div>
+  </div>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      dogId: '',
+      dogName: '',
+      imageUrl: '',
+    };
+  },
+  methods: {
+    loadRandomDog() {
+      fetch('/dogs')
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error('Could not load a dog');
+        })
+        .then((data) => {
+          this.dogName = data.name;
+          this.imageUrl = data.imageUrl;
+          this.dogId = data.id;
+        });
+    },
+    like() {
+      fetch('/dogs/likes', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          id: this.dogId,
+        }),
+      })
+        .then((response) => {
+          if (response.ok) {
+            this.loadRandomDog();
+          } else {
+            throw new Error('Could not like dog!');
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    dislike() {
+      fetch('/dogs/dislikes', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          id: this.dogId,
+        }),
+      })
+        .then((response) => {
+          if (response.ok) {
+            this.loadRandomDog();
+          } else {
+            throw new Error('Could not dislike dog!');
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  mounted() {
+    this.loadRandomDog();
+  },
+};
 </script>
+
+<style scoped>
+.dog-photo {
+  height: 400px;
+  width: auto;
+  border: 1px darkgray solid;
+  padding: 0;
+}
+</style>
